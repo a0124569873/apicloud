af0c1f9c4c23c72384b4b9381dfa35f924b46e17
zxn <zxn@qq.com>
2019-02-14 10:03:47 +0800

    first commit

---
+++ dist/precache-manifest.c3198f476eb5af31e4a143f1f57063e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "483b9a90f9019ca0eaf4",
+    "url": "js/index.6468da55.js"
+  },
+  {
+    "revision": "36abefb3310a6bc5768e",
+    "url": "js/chunk-vendors.e0ccf0a7.js"
+  },
+  {
+    "revision": "4d2ad1c4a6b01a6740c9",
+    "url": "js/about.504fd5a0.js"
+  },
+  {
+    "revision": "3040bce915201201d747cfc4af031bff",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "483b9a90f9019ca0eaf4",
+    "url": "css/index.80d62a31.css"
+  },
+  {
+    "revision": "36abefb3310a6bc5768e",
+    "url": "css/chunk-vendors.4a6dcd71.css"
+  },
+  {
+    "revision": "4d2ad1c4a6b01a6740c9",
+    "url": "css/about.399a7353.css"
+  }
+];
